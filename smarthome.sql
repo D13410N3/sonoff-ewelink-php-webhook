f8d9f8275d2e77dc6a9aaf2567e70031ca4d6bf6
root <admin@4nmv.ru>
2020-01-22 09:43:20 +0300

    Removed comments

--- smarthome.sql
+++ smarthome.sql
@@ -1,23 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.2
--- https://www.phpmyadmin.net/
---
--- Хост: localhost
--- Время создания: Янв 20 2020 г., 22:30
--- Версия сервера: 10.0.38-MariaDB-0ubuntu0.16.04.1
--- Версия PHP: 7.0.33-12+ubuntu16.04.1+deb.sury.org+1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- База данных: `smarthome`
 --
@@ -67,48 +47,26 @@ CREATE TABLE `rooms` (
   `deleted` tinyint(4) DEFAULT '0'
 ) ENGINE=Aria DEFAULT CHARSET=utf8;
 
---
--- Индексы сохранённых таблиц
---
 
---
--- Индексы таблицы `ewelink_devices`
---
 ALTER TABLE `ewelink_devices`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `ewelink_events`
---
+
 ALTER TABLE `ewelink_events`
   ADD PRIMARY KEY (`id`);
 
---
--- Индексы таблицы `rooms`
---
+
 ALTER TABLE `rooms`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
 
---
--- AUTO_INCREMENT для таблицы `ewelink_devices`
---
 ALTER TABLE `ewelink_devices`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `ewelink_events`
---
+
+  
 ALTER TABLE `ewelink_events`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `rooms`
---
+
+  
 ALTER TABLE `rooms`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
