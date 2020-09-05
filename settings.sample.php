c77d9373c836c06877d1cd6119eb7ae251712062
root <admin@4nmv.ru>
2020-09-05 11:14:19 +0300

    Moved some settings to database

--- settings.sample.php
+++ settings.sample.php
@@ -10,15 +10,3 @@ $_DATABASE['password'] = 'superpass';
 
 // имя базы
 $_DATABASE['db'] = 'smarthome';
-
-// токен для бота 
-$_TG['token'] = 'token_from_botfather';
-
-// id чата для уведомлений
-$_TG['chat'] = 'your_chat_id';
-
-// IFTTT Maker Key
-$_IFTTT['key'] = 'Enter IFTTT key';
-
-// пароль для доступа к сайту
-$_PASSWORD = 'Password to access';
